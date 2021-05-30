107304a024a35b60882745332c86ac08359a6f96
Can Doğramacı <can.dogramaci@hepsiburada.com>
2021-05-31 00:43:56 +0300

    .

--- common/Hash.js
+++ common/Hash.js
@@ -1,20 +1,28 @@
-const crypto = require('crypto'),
-    algorithm = 'aes-256-ctr',
-    password = 'd6F3Efeq';
-
-function encrypt(text){
-  var cipher = crypto.createCipheriv(algorithm,password)
-  var crypted = cipher.update(text,'utf8','hex')
-  crypted += cipher.final('hex');
-  return crypted;
-}
- 
-function decrypt(text){
-  var decipher = crypto.createDecipheriv(algorithm,password)
-  var dec = decipher.update(text,'hex','utf8')
-  dec += decipher.final('utf8');
-  return dec;
-}
+const crypto = require('crypto')
+const algorithm = 'aes-256-ctr';
+const secretKey = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3';
+const iv = crypto.randomBytes(16);
+    
+    const encrypt = (text) => {
+    
+        const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+    
+        const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+    
+        return {
+            iv: iv.toString('hex'),
+            content: encrypted.toString('hex')
+        };
+    };
+    
+    const decrypt = (hash) => {
+    
+        const decipher = crypto.createDecipheriv(algorithm, secretKey, Buffer.from(hash.iv, 'hex'));
+    
+        const decrpyted = Buffer.concat([decipher.update(Buffer.from(hash.content, 'hex')), decipher.final()]);
+    
+        return decrpyted.toString();
+    };
 module.exports = {
     encrypt,
     decrypt
